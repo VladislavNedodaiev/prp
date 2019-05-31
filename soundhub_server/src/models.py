5e83e38af15675f93cbb9ac2c9be8e713b49397a
Bogdan <31548234+OnshaBogdan@users.noreply.github.com>
2019-05-31 17:45:50 +0300

    Added src/models.py Contains description of db models.

---
+++ soundhub_server/src/models.py
@@ -0,0 +1,63 @@
+from peewee import *
+from datetime import datetime
+
+db = SqliteDatabase('soundhub.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    username = CharField(unique=True)
+    password_hash = CharField()
+    photo_path = CharField(null=True)
+    bio = TextField(null=True)
+    phone = CharField(null=True)
+    date_of_reg = DateField(datetime.date(datetime.now()))
+    email = CharField(null=True)
+    balance = FloatField(default=0)
+    is_premium = BooleanField(default=False)
+    premium_exp_date = DateField(null=True)
+
+
+class Track(BaseModel):
+    author = ForeignKeyField(User, backref="songs")
+    title = CharField()
+    date_of_pub = DateField(datetime.date(datetime.now()))
+    duration = SmallIntegerField(null=True)  # in seconds
+    filepath = CharField()
+
+
+class Playlist(BaseModel):
+    author = ForeignKeyField(User, backref="playlists")
+    title = CharField()
+    date_of_creation = DateField(datetime.date(datetime.now()))
+    last_update = DateField(null=True)
+    description = TextField(null=True)
+    photo_path = CharField(null=True)
+
+
+class PlaylistTracks(BaseModel):
+    track = ForeignKeyField(Track, backref="playlists", null=True)
+    playlist = ForeignKeyField(Playlist, backref="tracks")
+
+
+class Like(BaseModel):
+    track = ForeignKeyField(Track, backref="liked")
+    user = ForeignKeyField(User, backref="liked")
+
+
+class Genre(BaseModel):
+    title = CharField()
+
+
+class TrackGenre(BaseModel):
+    genre = ForeignKeyField(Genre, backref="track")
+    track = ForeignKeyField(Track, backref="genre", null=True)
+
+
+class Subscription(BaseModel):
+    subscriber = ForeignKeyField(User, backref="subscriptions", null=True)
+    subscribed_user = ForeignKeyField(User, backref="subscribers", null=True)
