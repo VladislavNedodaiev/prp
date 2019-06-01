6f3d7a11578d74bc434d22ea2d3b80333e8e0cb8
Bogdan <31548234+OnshaBogdan@users.noreply.github.com>
2019-06-01 23:32:20 +0300

    Add tests.py: stores tests for db.driver.py

---
+++ soundhub_server/src/tests.py
@@ -0,0 +1,76 @@
+import unittest
+from src.db_driver import Driver
+from src.models import *
+
+MODELS = [User, Track, Playlist, PlaylistTracks, Like, Genre, TrackGenre, Subscription]
+test_db = SqliteDatabase(':memory:')
+
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+
+        test_db.connect()
+        test_db.create_tables(MODELS)
+
+        user1 = User(
+            username='user1',
+            password_hash='12345678',
+            email="user1@gmail.com"
+        )
+
+        user2 = User(
+            username='user2',
+            password_hash='23456789',
+            email="user2@mail.com"
+        )
+
+        user3 = User(
+            username='user3',
+            password_hash='34567890',
+            email="user3@gmail.com"
+        )
+
+        user1.save()
+        user2.save()
+        user3.save()
+
+    def tearDown(self):
+        test_db.drop_tables(MODELS)
+        test_db.close()
+
+
+class TestUser(BaseTestCase):
+
+    def test_db_connection(self):
+        users_count = len(User.select())
+        self.assertEqual(users_count, 3)
+
+    def test_register(self):
+        driver = Driver()
+
+        user = driver.register('testusername', 'testpassword', 'testemail@gmail.com')
+        self.assertEqual(user, User.get(User.username == 'testusername'))
+
+    def test_register_exists(self):
+        driver = Driver()
+
+        with self.assertRaises(IntegrityError):
+            user1 = driver.register('same_username', 'testpassword1', 'testemail1@gmail.com')
+            user2 = driver.register('same_username', 'testpassword2', 'testemail2@gmail.com')
+            user1.save()
+            user2.save()
+
+    def test_auth(self):
+        driver = Driver()
+
+        user_password = 'sajb4aisNsa83'
+        user = driver.register('testusername', user_password, 'testemail@gmail.com')
+
+        self.assertEqual(driver.auth(user.username, user_password), True)
+        self.assertEqual(driver.auth(user.username, user.password_hash), False)
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUser)
+    unittest.TextTestRunner(verbosity=2).run(suite)
